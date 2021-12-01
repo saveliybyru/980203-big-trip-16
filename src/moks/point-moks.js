bb002c8e887c660b792bb71c930be67770f39c8b
Saveliy <saveliyburk@gmail.com>
2021-12-01 12:34:52 +0300

    Завершает генерацию с моками

--- src/moks/point-moks.js
+++ src/moks/point-moks.js
@@ -1,96 +1,90 @@
 import dayjs from 'dayjs';
 
+const pointsType=[
+  'taxi',
+  'bus',
+  'train',
+  'ship',
+  'drive',
+  'flight',
+  'check-in',
+  'sightseeing',
+  'restaurant',
+];
+
+const cities = [
+  'Valkla',
+  'Laagna',
+  'Rannakula',
+  'Viimsi',
+  'Laagri',
+  'Rapla',
+  'Viimsi',
+  'Lavassaare',
+  'Roosta',
+  'Viljandi',
+  'Laulasmaa',
+  'Ruusma',
+  'Vihterpalu',
+  'Lilby',
+  'Räpina',
+  'Vihula',
+  'Lihula',
+  'Vyru',
+  'Loksa',
+  'Saku',
+  'Vykhma',
+  'Lohusuu',
+  'Salme',
+  'Värska',
+  'Luunya',
+];
+
+const descriptions = [
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  'Cras aliquet varius magna, non porta ligula feugiat eget.',
+  'Fusce tristique felis at fermentum pharetra.',
+  'Aliquam id orci ut lectus varius viverra.',
+  'Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante.',
+  'Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum.',
+  'Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui.',
+  'Sed sed nisi sed augue convallis suscipit in sed felis.',
+  'Aliquam erat volutpat.',
+  'Nunc fermentum tortor ac porta dapibus.',
+  'In rutrum ac purus sit amet tempus.',
+];
 
 const getRandomInteger = (a = 0, b = 1) => {
   const lower = Math.ceil(Math.min(a, b));
   const upper = Math.floor(Math.max(a, b));
-
   return Math.floor(lower + Math.random() * (upper - lower + 1));
 };
 
 
-const generateTypePoint = () => {
-  const typesPoint=[
-    'taxi',
-    'bus',
-    'train',
-    'ship',
-    'drive',
-    'flight',
-    'check-in',
-    'sightseeing',
-    'restaurant',
-  ];
-  const randomIndex = getRandomInteger(0, typesPoint.length - 1);
-
-  return typesPoint[randomIndex];
+const generateTypePoint = (points) => {
+  const randomIndex = getRandomInteger(0, points.length - 1);
+  return points[randomIndex];
 };
 
-const generateCity = () => {
-  const city = [
-    'Valkla',
-    'Laagna',
-    'Rannakula',
-    'Viimsi',
-    'Laagri',
-    'Rapla',
-    'Viimsi',
-    'Lavassaare',
-    'Roosta',
-    'Viljandi',
-    'Laulasmaa',
-    'Ruusma',
-    'Vihterpalu',
-    'Lilby',
-    'Räpina',
-    'Vihula',
-    'Lihula',
-    'Vyru',
-    'Loksa',
-    'Saku',
-    'Vykhma',
-    'Lohusuu',
-    'Salme',
-    'Värska',
-    'Luunya',
-  ];
-  const randomIndex = getRandomInteger(0, city.length - 1);
-
-  return city[randomIndex];
+const generateCity = (cities) => {
+  const randomIndex = getRandomInteger(0, cities.length - 1);
+  return cities[randomIndex];
 };
 
-const generateDescription = () => {
-  const descriptions = [
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-    'Cras aliquet varius magna, non porta ligula feugiat eget.',
-    'Fusce tristique felis at fermentum pharetra.',
-    'Aliquam id orci ut lectus varius viverra.',
-    'Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante.',
-    'Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum.',
-    'Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui.',
-    'Sed sed nisi sed augue convallis suscipit in sed felis.',
-    'Aliquam erat volutpat.',
-    'Nunc fermentum tortor ac porta dapibus.',
-    'In rutrum ac purus sit amet tempus.',
-  ];
-  const DESCRIPTION_COUNT = getRandomInteger(1, 5);
+const generateDescription = (descriptions) => {
+  const descriptionCount = getRandomInteger(1, 5);
   let description = '';
-
-  for (let i = 0; i < DESCRIPTION_COUNT; i++) {
-
+  for (let i = 0; i < descriptionCount; i++) {
     const randomIndex = getRandomInteger(0, descriptions.length - 1);
-
     description = description + String(descriptions[randomIndex]);
   }
-
-
   return description;
 };
 
 const generatePhotos = () => {
-  const PHOTO_COUNT = getRandomInteger(1, 8);
+  const photosCount = getRandomInteger(1, 8);
   const photos = [];
-  for (let i = 0; i < PHOTO_COUNT; i++) {
+  for (let i = 0; i < photosCount; i++) {
     photos.push(`http://picsum.photos/248/152?r=${Math.random()}`);
   }
   return photos;
@@ -107,25 +101,25 @@ const generateDateTime = () => {
   const timeGap = getRandomInteger(-MINUTEGAP, MINUTEGAP);
   const startTimeUnformat = dayjs(gapToday).add(timeGap, 'm').toDate();
   const startTime = dayjs(startTimeUnformat).format('HH:mm');
-  const endTimeUnformat = dayjs(startTimeUnformat).add(timeGap, 'm');
+  const endTimeUnformat = dayjs(startTimeUnformat).add(timeGap, 'm').toDate();
   const endTime = dayjs(endTimeUnformat).format('HH:mm');
-  const timePeriod = 10;
+  const timePeriodUnformat = dayjs(startTimeUnformat).diff(dayjs(endTimeUnformat));
+  const timePeriod = dayjs(timePeriodUnformat).format('HH[H] mm[M]');
 
   const time = [date, startTime, endTime, timePeriod];
 
   return time;
 };
 
-
 const generatePoint = () => {
   const point = {
     date: generateDateTime()[0],
     continueTime: generateDateTime()[3],
-    type: generateTypePoint(),
-    city: generateCity(),
+    type: generateTypePoint(pointsType),
+    city: generateCity(cities),
     price: getRandomInteger(100, 1000),
     options:'2',
-    description: generateDescription(),
+    description: generateDescription(descriptions),
     photo: generatePhotos(),
     timeStart: generateDateTime()[1],
     timeEnd: generateDateTime()[2],
--- src/view/event-edit-form-view.js
+++ src/view/event-edit-form-view.js
@@ -1,6 +1,39 @@
+const pointsType=[
+  'taxi',
+  'bus',
+  'train',
+  'ship',
+  'drive',
+  'flight',
+  'check-in',
+  'sightseeing',
+  'restaurant',
+];
+
+
 const createEventEditFormTemplate = (point) => {
 
-  const {description} = point;
+  const {description, date, type, city, price,  timeStart, timeEnd} = point;
+
+
+  const checkType = (actualType, typeList) => {
+    let list = '';
+    for (const type of typeList) {
+      if (type === actualType){
+        list += `<div class="event__type-item">
+        <input id="event-type-${type}-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="${type}" checked>
+        <label class="event__type-label  event__type-label--${type}" for="event-type-${type}-1">${type}</label>
+      </div>`
+      }
+      else {
+        list +=`<div class="event__type-item">
+        <input id="event-type-${type}-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="${type}">
+        <label class="event__type-label  event__type-label--${type}" for="event-type-${type}-1">${type}</label>
+      </div>`
+      };
+    };
+    return list;
+  };
 
   return `<li class="trip-events__item">
   <form class="event event--edit" action="#" method="post">
@@ -8,67 +41,23 @@ const createEventEditFormTemplate = (point) => {
       <div class="event__type-wrapper">
         <label class="event__type  event__type-btn" for="event-type-toggle-1">
           <span class="visually-hidden">Choose event type</span>
-          <img class="event__type-icon" width="17" height="17" src="img/icons/flight.png" alt="Event type icon">
+          <img class="event__type-icon" width="17" height="17" src="img/icons/${type}.png" alt="Event type icon">
         </label>
         <input class="event__type-toggle  visually-hidden" id="event-type-toggle-1" type="checkbox">
 
         <div class="event__type-list">
           <fieldset class="event__type-group">
             <legend class="visually-hidden">Event type</legend>
-
-            <div class="event__type-item">
-              <input id="event-type-taxi-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="taxi">
-              <label class="event__type-label  event__type-label--taxi" for="event-type-taxi-1">Taxi</label>
-            </div>
-
-            <div class="event__type-item">
-              <input id="event-type-bus-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="bus">
-              <label class="event__type-label  event__type-label--bus" for="event-type-bus-1">Bus</label>
-            </div>
-
-            <div class="event__type-item">
-              <input id="event-type-train-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="train">
-              <label class="event__type-label  event__type-label--train" for="event-type-train-1">Train</label>
-            </div>
-
-            <div class="event__type-item">
-              <input id="event-type-ship-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="ship">
-              <label class="event__type-label  event__type-label--ship" for="event-type-ship-1">Ship</label>
-            </div>
-
-            <div class="event__type-item">
-              <input id="event-type-drive-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="drive">
-              <label class="event__type-label  event__type-label--drive" for="event-type-drive-1">Drive</label>
-            </div>
-
-            <div class="event__type-item">
-              <input id="event-type-flight-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="flight" checked>
-              <label class="event__type-label  event__type-label--flight" for="event-type-flight-1">Flight</label>
-            </div>
-
-            <div class="event__type-item">
-              <input id="event-type-check-in-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="check-in">
-              <label class="event__type-label  event__type-label--check-in" for="event-type-check-in-1">Check-in</label>
-            </div>
-
-            <div class="event__type-item">
-              <input id="event-type-sightseeing-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="sightseeing">
-              <label class="event__type-label  event__type-label--sightseeing" for="event-type-sightseeing-1">Sightseeing</label>
-            </div>
-
-            <div class="event__type-item">
-              <input id="event-type-restaurant-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="restaurant">
-              <label class="event__type-label  event__type-label--restaurant" for="event-type-restaurant-1">Restaurant</label>
-            </div>
+            ${checkType(point.type, pointsType)}
           </fieldset>
         </div>
       </div>
 
       <div class="event__field-group  event__field-group--destination">
         <label class="event__label  event__type-output" for="event-destination-1">
-          Flight
+        ${type}
         </label>
-        <input class="event__input  event__input--destination" id="event-destination-1" type="text" name="event-destination" value="Chamonix" list="destination-list-1">
+        <input class="event__input  event__input--destination" id="event-destination-1" type="text" name="event-destination" value="${city}" list="destination-list-1">
         <datalist id="destination-list-1">
           <option value="Amsterdam"></option>
           <option value="Geneva"></option>
@@ -78,10 +67,10 @@ const createEventEditFormTemplate = (point) => {
 
       <div class="event__field-group  event__field-group--time">
         <label class="visually-hidden" for="event-start-time-1">From</label>
-        <input class="event__input  event__input--time" id="event-start-time-1" type="text" name="event-start-time" value="18/03/19 12:25">
+        <input class="event__input  event__input--time" id="event-start-time-1" type="text" name="event-start-time" value="${date} ${timeStart}">
         &mdash;
         <label class="visually-hidden" for="event-end-time-1">To</label>
-        <input class="event__input  event__input--time" id="event-end-time-1" type="text" name="event-end-time" value="18/03/19 13:35">
+        <input class="event__input  event__input--time" id="event-end-time-1" type="text" name="event-end-time" value="${date} ${timeEnd}">
       </div>
 
       <div class="event__field-group  event__field-group--price">
@@ -89,7 +78,7 @@ const createEventEditFormTemplate = (point) => {
           <span class="visually-hidden">Price</span>
           &euro;
         </label>
-        <input class="event__input  event__input--price" id="event-price-1" type="text" name="event-price" value="160">
+        <input class="event__input  event__input--price" id="event-price-1" type="text" name="event-price" value="${price}">
       </div>
 
       <button class="event__save-btn  btn  btn--blue" type="submit">Save</button>
